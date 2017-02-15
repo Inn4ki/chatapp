160e918145d218e3e2666f9b3f2a6a49f261b626
Inn4ki <ilacaci@gmail.com>
2017-02-16 00:15:10 +0100

    initial

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ var express = require("express");
 var app = express();
 
 app.use(express.static("public"));
+app.use(express.static("node/modules/bootstrap/dist"));
 
 app.get('/', function (req, res) {
     res.send("Hello World");
@@ -11,5 +12,3 @@ app.listen(3000, function () {
     console.log('Chatt app listening on port 3000!');
 });
 
-/*
-+  ++*/
