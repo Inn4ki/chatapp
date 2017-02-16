c57d55766bc2036badeb9255a9f4b9585a8dafa7
Inn4ki <ilacaci@gmail.com>
2017-02-16 03:38:36 +0100

    bootstrap from WS

--- app.js
+++ app.js
@@ -1,12 +1,11 @@
 var express = require("express");
 var app =  express();
 
+app.use(express.static("public"));
+app.use(express.static("node_modules/bootstrap/dist"));
+
 app.get("/", function (req, res) {
-<<<<<<< HEAD
     res.send("Hello Worlds");
-=======
-    res.send("Hello World");
->>>>>>> b8142091801b1681addd56108d4350e13b155c9d
 });
 
 app.listen(3000, function () {
