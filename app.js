8d3084d06ef07e2c4535e8c56fd33efe01df1156
Inn4ki <ilacaci@gmail.com>
2017-02-18 00:53:03 +0100

    jquery

--- app.js
+++ app.js
@@ -1,24 +1,27 @@
 var express = require("express");
 var app =  express();
+var bodyParser =  require("body-parser");
 
 app.set("views", "./views");
-app.set("view engine", 'pug');
+app.set('view engine', 'pug');
 
 app.use(express.static("public"));
 app.use(express.static("node_modules/bootstrap/dist"));
+app.use(express.static("node_modules/jquery/dist"));
+
+app.use(bodyParser.urlencoded({ extended:   true }));
 
 app.get('/', function (req, res) {
-    res.render("index", {title: "Home"});
+    res.render("home", { title: "Home"});
 });
 
-app.get('/admin/rooms', function (req, res){
-    res.render("rooms", {title: "Chat Rooms"});
-});
+var adminRouter = require("./admin");
+app.use("/admin", adminRouter);
 
-app.get('/help', function (req, res) {
-    res.render("rooms");
-});
+var apiRouter = require("./api");
+app.use("/admin", apiRouter);
+
 
 app.listen(3000, function () {
-    console.log('Example app listening on port 3000');
+    console.log('Chat app listening on port 3000!');
 });
