d2bb1a530147e75707e2f62fab47f948ec6f54c2
Inn4ki <ilacaci@gmail.com>
2017-02-17 17:25:37 +0100

    jade -> pug

--- app.js
+++ app.js
@@ -1,11 +1,22 @@
 var express = require("express");
 var app =  express();
 
+app.set("views", "./views");
+app.set("view engine", 'pug');
+
 app.use(express.static("public"));
 app.use(express.static("node_modules/bootstrap/dist"));
 
+app.get('/', function (req, res) {
+    res.render("index", {title: "Home"});
+});
+
+app.get('/admin/rooms', function (req, res){
+    res.render("rooms", {title: "Chat Rooms"});
+});
+
 app.get('/hello', function (req, res) {
-    res.render("rooms.pug");
+    res.render("rooms");
 });
 
 app.listen(3000, function () {
