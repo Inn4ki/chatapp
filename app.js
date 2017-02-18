cecb54730359f34e10c0cf5b42318b435e831b4f
Inn4ki <ilacaci@gmail.com>
2017-02-18 15:02:35 +0100

    lodash

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 var express = require("express");
-var app =  express();
-var bodyParser =  require("body-parser");
+var app = express();
+var bodyParser = require("body-parser");
 
 app.set("views", "./views");
 app.set('view engine', 'pug');
@@ -10,6 +10,7 @@ app.use(express.static("node_modules/bootstrap/dist"));
 app.use(express.static("node_modules/jquery/dist"));
 
 app.use(bodyParser.urlencoded({ extended:   true }));
+app.use(bodyParser.json());
 
 app.get('/', function (req, res) {
     res.render("home", { title: "Home"});
@@ -19,8 +20,7 @@ var adminRouter = require("./admin");
 app.use("/admin", adminRouter);
 
 var apiRouter = require("./api");
-app.use("/admin", apiRouter);
-
+app.use("/api", apiRouter);
 
 app.listen(3000, function () {
     console.log('Chat app listening on port 3000!');
