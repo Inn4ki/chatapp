006a35e75f34618150085fb2f564cc5fe553d988
Inn4ki <ilacaci@gmail.com>
2017-02-18 14:31:39 +0100

    lodash

--- api.js
+++ api.js
@@ -2,7 +2,7 @@ var express = require("express");
 var rooms = require("./data/rooms.json");
 var messages = require("./data/messages.json");
 var _ = require("lodash");
-var uuid = require("node-uuid");
+
 
 var router = express.Router();
 module.exports = router;
@@ -10,6 +10,7 @@ module.exports = router;
 router.get("/rooms", function (req, res) {
     res.json(rooms);
 });
+
 
 router.route("/rooms/:roomId/messages")
     .get(function (req, res) {
