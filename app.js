b67d172879c5e8be5b2034e747d214094fb7c991
Inn4ki <ilacaci@gmail.com>
2017-02-18 16:48:10 +0100

    lodash

--- app.js
+++ app.js
@@ -2,12 +2,21 @@ var express = require("express");
 var app = express();
 var bodyParser = require("body-parser");
 
+
 app.set("views", "./views");
 app.set('view engine', 'pug');
+
+app.use(require("morgan") ("combined"));
+
+/*npm WARN deprecated transformers@1.8.3: Deprecated, use jstransformer
+ chatapp@1.0.0 /home/inn4ki/WebstormProjects/chatapp
+ └─┬ express-debug@1.1.1 */
 
 app.use(express.static("public"));
 app.use(express.static("node_modules/bootstrap/dist"));
 app.use(express.static("node_modules/jquery/dist"));
+
+require('express-debug')(app, {});
 
 app.use(bodyParser.urlencoded({ extended:   true }));
 app.use(bodyParser.json());
